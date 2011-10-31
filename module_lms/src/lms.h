419c2d0682e9fe10e9f0b992254c342d9ce34764
Mikael <micke.lilltroll@hotmail.com>
2011-10-31 13:22:20 +0100

    Added lms.h
    Signed-off-by: Mikael <micke.lilltroll@hotmail.com>

---
+++ module_lms/src/lms.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011, Mikael Bohman, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
+
+#ifndef LMS_H_
+#define LMS_H_
+int lms(int x_ref, int x_des, int mu,int coeffs[], int state[],int ELEMENTS);
+
+#endif /* LMS_H_ */
